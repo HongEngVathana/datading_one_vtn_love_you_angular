a37b770cc801367c711aa52712c1c1aa12eb7157
HongEngVathana <engvathanahong@gmail.com>
2024-09-06 15:45:08 +0700

    for  you all developer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+ @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'databiding';
+  title:string="This is me VTN"
+  vathana:number=23*1000
+  imgurl:string="https://i.pinimg.com/236x/ff/77/c6/ff77c6790d107dd168f5458597e83af3.jpg"
+  idDisable:boolean=true
+  isActive:boolean=true
+  fruitName:string="Apple"
+  
 }
